d72896aff9bbe26a1b37518884ffa869d8524806
termokop <termokop564@gmail.com>
2023-01-09 17:30:52 +0400

    add_draw

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -54,20 +54,20 @@ export class Game extends React.Component <{}, {stepNumber:number, history:any, 
       })
 
       let status = 'Next player ' + (this.state.xIsNext ? 'X' : 'O')
-      if(winner) status = 'Winner is ' + winner
-
+      if(winner === 'X' || winner === 'O') status = 'Winner is ' + winner
+      if(winner === 'draw') status = 'Draw'
       return (
         
         <div className='bg-gray-900 h-screen w-screen flex flex-wrap content-start'>
           <h1 className='text-center text-yellow-500 text-5xl w-full m-5 my-20'>tic-tac-toe</h1>
-          <div className=' w-1/4 text-center text-white text-xl p-3'>{status}</div>
-          <div className="p-2 m-0 w-1/2">
+          <div className=' w-1/4 min-w-fit text-center text-white text-xl p-3'>{status}</div>
+          <div className="p-2 m-0 w-1/2 ">
             <Board 
               squares={current.squares}
               onClick={(i:number) => this.handleClick(i)}
             />
           </div>
-          <div className=" w-1/4 text-center text-white text-xl p-3">
+          <div className=" w-1/4 min-w-fit text-center text-white text-xl p-3">
             <ol className='w-4/5'>{moves}</ol>
           </div>
           
@@ -87,11 +87,14 @@ export class Game extends React.Component <{}, {stepNumber:number, history:any, 
       [0, 4, 8],
       [2, 4, 6],
     ];
+    let draw = 0
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
+      if(squares[a] !== null && squares[b] !== null && squares[c] !== null)draw++
+      if(draw === 8) return 'draw'
     }
     return null;
   }
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export function Square(props:any) {
     return (
       <button className='bg-yellow-200 h-20 w-20 m-1 text-7xl text-green-600' onClick={props.onClick}>
